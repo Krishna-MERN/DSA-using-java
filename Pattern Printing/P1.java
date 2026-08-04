529d2b2540a6ce759838656ebdfdfa10eb41ebbb
Krishna-MERN <mail.kkshukla@gmail.com>
2026-08-04 18:57:48 +0530

    second commit

---
+++ Pattern Printing/P1.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+// 04/08/2026   
+
+// Printing the rectunguler star.
+
+
+public class P1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter no of Rows");
+        int rows = sc.nextInt();
+        System.out.println("Enter no of Rows");
+        int column = sc.nextInt();
+        for (int i = 0 ; i < rows ; i++){
+            for (int j = 0 ; j < column ; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}

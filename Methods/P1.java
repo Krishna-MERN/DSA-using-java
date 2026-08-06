6ba6f6291fdb9b46a9d6d4f68344f2ba94739724
Krishna-MERN <mail.kkshukla@gmail.com>
2026-08-06 21:57:15 +0530

    3rd day 2nd commit

---
+++ Methods/P1.java
@@ -0,0 +1,9 @@
+package Methods;
+
+public class P1 {
+    public static void main(String[] args){
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.cbrt(64));
+        System.out.println(Math.max(3,6));
+    }
+}
